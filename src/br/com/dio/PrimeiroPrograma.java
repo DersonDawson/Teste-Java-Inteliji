9803fbe7f1189a4388be80eaf718420006995384
Anderson <andersonsmurf@gmail.com>
2021-11-29 07:12:35 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        Object gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello Wolrd! " + (a+b));*/
+    }
+}
